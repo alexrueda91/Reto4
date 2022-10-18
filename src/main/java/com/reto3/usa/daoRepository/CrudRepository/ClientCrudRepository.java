41c6451f2e8d99ac17da9f97e6f8d3c03585332e
alexrueda91 <113567006+alexrueda91@users.noreply.github.com>
2022-10-17 23:21:39 -0500

    Add files via upload

---
+++ src/main/java/com/reto3/usa/daoRepository/CrudRepository/ClientCrudRepository.java
@@ -0,0 +1,8 @@
+package com.reto3.usa.daoRepository.CrudRepository;
+
+import com.reto3.usa.model.Client;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientCrudRepository extends CrudRepository<Client, Integer> {
+
+}
---
+++ src/main/java/com/reto3/usa/daoRepository/CrudRepository/ScoreCrudRepository.java
@@ -0,0 +1,8 @@
+package com.reto3.usa.daoRepository.CrudRepository;
+
+import com.reto3.usa.model.Score;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ScoreCrudRepository extends CrudRepository<Score, Integer> {
+
+}
